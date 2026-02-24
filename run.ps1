f51d9e795f6748df0022b9dc3bd217f930aa7b99
JMJAJ <34998292+JMJAJ@users.noreply.github.com>
2026-02-24 17:38:53 +0100

    fixed latest

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $exePath = "$env:TEMP\movcli.exe"
-$url = "https://github.com/JMJAJ/movcli/releases/download/v1.0.0/movcli.exe"
+$url = "https://github.com/JMJAJ/movcli/releases/latest/download/movcli.exe"
 
 Write-Host "🍿 Grabbing the popcorn (Loading MovCLI)..." -ForegroundColor Cyan
 Invoke-RestMethod -Uri $url -OutFile $exePath -UseBasicParsing
